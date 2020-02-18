3568454554dca028168ef03e629448ee70ae87f6
z4yx <z4yx@users.noreply.github.com>
2020-02-18 22:55:50 +0800

    setup new dev environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,54 @@
 'use strict';
 
-// get port from environment settings for deployment on Heroku
-var EXPRESS_PORT = process.env.PORT || 4000;
-var EXPRESS_ROOT = __dirname;
-var LIVERELOAD_PORT = 35729;
- 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var log = gutil.log;
-
-var jshint = require('gulp-jshint');
-
+var ts = require("gulp-typescript");
+var browserify = require("browserify");
+var vinyl_source = require('vinyl-source-stream');
+var tsify = require("tsify");
+var tsProject = ts.createProject("tsconfig.json");
+// var jshint = require('gulp-jshint');
 var zip = require('gulp-zip');
 
-var mocha = require('gulp-mocha');
-
+var log = gutil.log;
 var codeFiles = ['js/**/*.js', '!test/**/*.js', '!node_modules/**'];
 var testFiles = ['test/**/*.js'];
 
-gulp.task('lint', function(){
-  log('Linting Files');
-  gulp.src(codeFiles)
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
+// gulp.task("tsc", function () {
+//   return tsProject.src()
+//     .pipe(tsProject())
+//     .js.pipe(gulp.dest("build"));
+// });
+
+gulp.task("tsc", function () {
+  return browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/gauth-api.ts'],
+    cache: {},
+    packageCache: {}
+  })
+    .plugin(tsify)
+    // .transform('babelify', {
+    //   presets: ['es2015'],
+    //   extensions: ['.ts']
+    // })
+    .bundle()
+    .pipe(vinyl_source('bundle.js'))
+    .pipe(gulp.dest("js"));
 });
 
-gulp.task('test', function(){
-  log('Running mocha tests');
-  gulp.src(testFiles)
-    .pipe(mocha({reporter: 'spec'}));
-});
+// gulp.task('lint', function(){
+//   log('Linting Files');
+//   gulp.src(codeFiles)
+//     .pipe(jshint('.jshintrc'))
+//     .pipe(jshint.reporter('default'))
+//     .pipe(jshint.reporter('fail'));
+// });
 
 gulp.task('watch', function(){
   log('Watching Files');
-  gulp.watch(codeFiles, ['lint']);
-});
-
-function startExpress(root, port) {
-  var express = require('express');
-  var app = express();
-  app.use(require('connect-livereload')());
-  app.use(express.static(root));
-  app.listen(port, function() {
-    log('Listening on port:', port);
-  });
-}
-
-var lr;
-function startLivereload(port) {
-  lr = require('tiny-lr')();
-  lr.listen(port);
-}
- 
-function notifyLivereload(event) {
-  gulp.src(event.path, {read: false})
-      .pipe(require('gulp-livereload')(lr));
-}
-
-gulp.task('server', function () {
-  startExpress(EXPRESS_ROOT, EXPRESS_PORT);
-  startLivereload(LIVERELOAD_PORT);
-  gulp.watch(['*.html', 'js/**/*.js'], notifyLivereload);
+  gulp.watch(['src/*.ts'], gulp.series('tsc'));
 });
 
 gulp.task('makepkg', function () {
@@ -71,4 +58,4 @@ gulp.task('makepkg', function () {
     pipe(gulp.dest('.'));
 });
 
-gulp.task('default', ['lint', 'test']);
+// gulp.task('default', ['tsc']);
